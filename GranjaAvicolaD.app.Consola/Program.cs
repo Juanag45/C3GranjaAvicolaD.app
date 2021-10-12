698821b01fd343375349a5d58d785737198c2d2c
guillermo <juan_gui.alzate@uao.edu.co>
2021-10-12 08:00:10 -0500

    funciona y tiene mas funciones del backend

--- GranjaAvicolaD.app.Consola/Program.cs
+++ GranjaAvicolaD.app.Consola/Program.cs
@@ -8,10 +8,15 @@ namespace GranjaAvicolaD.app.Consola
     class Program
     {
         private static IRepositorioUsuario _repoUsuario=new RepositorioUsuario();
+        private static IRepositorioGalpon _repoGalpon=new RepositorioGalpon();
+
+        private static IRepositorioInicioSesion _repoDatosInicioSesion=new RepositorioInicioSesion();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            AddUsuario();
+            //AddUsuario();
+            AddGalpon();
+            AddDatosInicioSesion();
            // BuscarUsuario(1);
            // EliminarUsuario(2);
           // MostrarUsuarios();
@@ -20,9 +25,9 @@ namespace GranjaAvicolaD.app.Consola
         private static void AddUsuario()
         {
             var usuario = new Usuario{
-                Nombre="juan",
+                Nombre="Amor",
                 Correo="@gmail",
-                Telefono="6777775"
+                Telefono="105556655"
             };
             _repoUsuario.AddUsuario(usuario);
 
@@ -49,5 +54,73 @@ namespace GranjaAvicolaD.app.Consola
             }
         }
 
+
+
+
+        private static void AddGalpon()
+        {
+            var galpon = new Galpon{
+                NumeroGallinas=1000,
+                Ubicacion="1",
+            };
+            _repoGalpon.AddGalpon(galpon);
+
+        }
+
+        // private static void BuscarUsuario(int idUsuario)
+        // {
+        //     var usuario = _repoUsuario.GetUsuario(idUsuario);
+        //     Console.WriteLine(usuario.Nombre);
+        // }
+
+        // private static void EliminarUsuario(int idUsuario)
+        // {
+        //     _repoUsuario.DeleteUsuario(idUsuario);
+        //     Console.WriteLine("Usuario Eliminado");
+        // }
+
+        // private static void MostrarUsuarios()
+        // {
+        //     IEnumerable<Usuario> usuarios = _repoUsuario.GetAllUsuario();
+        //     foreach (var usuario in usuarios)
+        //     {
+        //         Console.WriteLine(usuario.Nombre + " " + usuario.Correo);
+        //     }
+        // }
+
+
+
+
+        private static void AddDatosInicioSesion()
+        {
+            var datosInicioSesion = new DatosInicioSesion{
+                UserName="Amaroo",
+                Pasword="hola",
+                Rol=Roles.veterinario
+            };
+            _repoDatosInicioSesion.AddDatosInicioSesion(datosInicioSesion);
+
+        }
+
+        // private static void BuscarUsuario(int idUsuario)
+        // {
+        //     var usuario = _repoUsuario.GetUsuario(idUsuario);
+        //     Console.WriteLine(usuario.Nombre);
+        // }
+
+        // private static void EliminarUsuario(int idUsuario)
+        // {
+        //     _repoUsuario.DeleteUsuario(idUsuario);
+        //     Console.WriteLine("Usuario Eliminado");
+        // }
+
+        // private static void MostrarUsuarios()
+        // {
+        //     IEnumerable<Usuario> usuarios = _repoUsuario.GetAllUsuario();
+        //     foreach (var usuario in usuarios)
+        //     {
+        //         Console.WriteLine(usuario.Nombre + " " + usuario.Correo);
+        //     }
+        // }
     }
 }
